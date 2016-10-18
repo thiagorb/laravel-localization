7e0eeb44f4c0f62894d0a85b510775cc6d147b7a
Thiago Barcala <thiago.barcala@gmail.com>
2016-10-18 18:09:57 +0200

    Refactor tests

--- test.js
+++ test.js
@@ -1,28 +1,41 @@
 import assert from 'assert';
-import Lang from './src/index';
 
-it('should translate messages', () => {
-    Lang.addMessages({
-		'en': {
-	        'message key': 'translated message',
-	        'different one': 'another translation'
-		}
-    });
+const MODULE_PATH = './src/index';
+
+suite('laravel-localization', () => {
+	let Lang;
 	
-	Lang.setLocale('en');
+	setup(() => {
+		Lang = require(MODULE_PATH).default;
+	});
+		
+	teardown(() => {
+		delete require.cache[require.resolve(MODULE_PATH)];
+	});
 
-    assert.equal(Lang.get('message key'), 'translated message');
-    assert.equal(Lang.get('different one'), 'another translation');
-});
-
-it('should translate messages with parameters', () => {
-    Lang.addMessages({
-		'en': {
-			'message with parameter: :parameter': 'translation with parameter: :parameter'
-		}
-    });
+	test('should translate messages', () => {
+	    Lang.addMessages({
+			'en': {
+		        'message key': 'translated message',
+		        'different one': 'another translation'
+			}
+	    });
 	
-	Lang.setLocale('en');
+		Lang.setLocale('en');
 
-    assert.equal(Lang.get('message with parameter: :parameter', { parameter: 'value' }), 'translation with parameter: value');
+	    assert.equal(Lang.get('message key'), 'translated message');
+	    assert.equal(Lang.get('different one'), 'another translation');
+	});
+
+	test('should translate messages with parameters', () => {
+	    Lang.addMessages({
+			'en': {
+				'message with parameter: :parameter': 'translation with parameter: :parameter'
+			}
+	    });
+	
+		Lang.setLocale('en');
+
+	    assert.equal(Lang.get('message with parameter: :parameter', { parameter: 'value' }), 'translation with parameter: value');
+	});
 });
